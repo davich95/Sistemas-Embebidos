183c4d6af80ac52c0e38936dbb937686ad5fd4c2
David <aguilardavid33@yahoo.es>
2016-05-20 14:31:01 -0500

    Update main.c

--- main/main.c
+++ main/main.c
@@ -2,7 +2,7 @@
  * main.c
  *
  * Created: 14/5/2016 18:33:15
- * Author : David
+ * Author : David Aguilar - Hugo Rengifo - Paul Armijos
  */ 
 
 #include <avr/io.h>
@@ -10,6 +10,7 @@
 
 int main(void)
 {
+	// Ahora se está tratando de hacer la interfaz del teclado con el micro y el lcd
 	DDRA = 0b00001111;
 	PORTA = 0b11111111;
     while (1) 
