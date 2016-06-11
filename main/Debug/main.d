5fe618e8601b7b5cccefea8744b34ec37ab0f577
davich95 <aguilardavid33@yahoo.es>
2016-06-11 13:24:07 -0500

    Update Cerradura
    Este codigo es el que actualmente estamos usando, debemos cambiarle la
    lógica para meter un algoritmo generador de claves que estoy
    desarrollando

--- main/Debug/main.d
+++ main/Debug/main.d
@@ -10,15 +10,12 @@ main.d main.o: .././main.c \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\4.9.2\include\stddef.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdio.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\4.9.2\include\stdarg.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h \
  .././lcd.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\pgmspace.h
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\pgmspace.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\4.9.2\include\stddef.h
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h:
 
@@ -42,14 +39,6 @@ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h:
 
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h:
-
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\4.9.2\include\stddef.h:
-
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdio.h:
-
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\4.9.2\include\stdarg.h:
-
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h:
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h:
@@ -59,3 +48,5 @@ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\
 .././lcd.h:
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\pgmspace.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\4.9.2\include\stddef.h:
--- main/main.c
+++ main/main.c
@@ -6,50 +6,24 @@
  */ 
 #define F_CPU 8000000UL
 #include <avr/io.h>
-#include <stdlib.h>
-#include <stdio.h>
 #include <util/delay.h>
 #include "lcd.h"
-int keypad  (void)
 { int a;
-	PORTB=1;
 		a=PINB&0b11110000;
 		a=a>>4;
-		if(a==1) return '7';//-7-	
-		if(a==2) return '8';//-8-
-		if(a==4) return '9';//-8-
-		if(a==8) return 'C';//-/-
-	PORTB=2;
 		a=PINB&0b11110000;
 		a=a>>4;
-		if(a==1) return '4';//-4-
-		if(a==2) return '5';//-5-
-		if(a==4) return '6';//-6-
-		if(a==8) return 'B';//*
-	PORTB=4;
 		a=PINB&0b11110000;
 		a=a>>4;
-		if(a==1) return '1';//-1-
-		if(a==2) return '2';//-2-``
-		if(a==4) return '3';//-3-
-		if(a==8) return 'A';
-	PORTB=8;
 		a=PINB&0b11110000;
 		a=a>>4;
-		if(a==1) return '#';//-on/c-
-		if(a==2) return '0';//-0-
-		if(a==4) return '*';//-=-
-		if(a==8) return 'D';//+
 	_delay_ms(50);
 	return 20;	
 }
 int main(void)
 {
-	lcd_init(LCD_DISP_ON_CURSOR_BLINK);
 	while(1)
 	{
-		lcd_clrscr();
-		lcd_puts("Ingrese dato");
 		_delay_ms(200);
 	}
 }
